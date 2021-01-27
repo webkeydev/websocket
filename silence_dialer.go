262b80a0f9abd08a4a663df06ef5f3c85076845b
Rudolf Tokic <rudolf.tokic@bitotron.com>
2021-01-27 17:39:22 +0100

    Remove unused silence dialer attributes

--- silence_dialer.go
+++ silence_dialer.go
@@ -5,37 +5,11 @@
 package websocket
 
 import (
-	"context"
-	"crypto/tls"
 	"net"
-	"net/http"
-	"net/url"
-	"time"
 )
 
 // A SilenceDialer contains options for connecting to WebSocket server.
 type SilenceDialer struct {
-	// NetDial specifies the dial function for creating TCP connections. If
-	// NetDial is nil, net.Dial is used.
-	NetDial func(network, addr string) (net.Conn, error)
-
-	// NetDialContext specifies the dial function for creating TCP connections. If
-	// NetDialContext is nil, net.DialContext is used.
-	NetDialContext func(ctx context.Context, network, addr string) (net.Conn, error)
-
-	// Proxy specifies a function to return a proxy for a given
-	// Request. If the function returns a non-nil error, the
-	// request is aborted with the provided error.
-	// If Proxy is nil or returns a nil *URL, no proxy is used.
-	Proxy func(*http.Request) (*url.URL, error)
-
-	// TLSClientConfig specifies the TLS configuration to use with tls.Client.
-	// If nil, the default configuration is used.
-	TLSClientConfig *tls.Config
-
-	// HandshakeTimeout specifies the duration for the handshake to complete.
-	HandshakeTimeout time.Duration
-
 	// ReadBufferSize and WriteBufferSize specify I/O buffer sizes in bytes. If a buffer
 	// size is zero, then a useful default size is used. The I/O buffer sizes
 	// do not limit the size of the messages that can be sent or received.
@@ -51,28 +25,11 @@ type SilenceDialer struct {
 	// Applications should use a single pool for each unique value of
 	// WriteBufferSize.
 	WriteBufferPool BufferPool
-
-	// Subprotocols specifies the client's requested subprotocols.
-	Subprotocols []string
-
-	// EnableCompression specifies if the client should attempt to negotiate
-	// per message compression (RFC 7692). Setting this value to true does not
-	// guarantee that compression will be supported. Currently only "no context
-	// takeover" modes are supported.
-	EnableCompression bool
-
-	// Jar specifies the cookie jar.
-	// If Jar is nil, cookies are not sent in requests and ignored
-	// in responses.
-	Jar http.CookieJar
 }
 
 // Dial creates a new client connection by calling DialContext with a background context.
 // DefaultDialer is a dialer with all fields set to the default values.
-var DefaultSilenceDialer = &SilenceDialer{
-	Proxy:            http.ProxyFromEnvironment,
-	HandshakeTimeout: 45 * time.Second,
-}
+var DefaultSilenceDialer = &SilenceDialer{}
 
 // nilDialer is dialer to use when receiver is nil.
 var nilSilenceDialer = *DefaultSilenceDialer
